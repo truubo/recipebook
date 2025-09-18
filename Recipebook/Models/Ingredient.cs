8ed29a61e6e5539463ff767701c5395731c5e317
Trubo <78948819+truubo@users.noreply.github.com>
2025-09-18 10:33:40 -0500

    Create models for Ingredient and Ingredient-Recipe join table. Waiting for recipe model

---
+++ Recipebook/Models/Ingredient.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Recipebook.Models
+{
+    public class Ingredient
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<IngredientRecipe> IngredientRecipes { get; set; }
+    }
+}
