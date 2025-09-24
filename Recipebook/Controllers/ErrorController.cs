d998977b6bd59e4ae04e594b6dbacfb4efe199f4
Trubo <78948819+truubo@users.noreply.github.com>
2025-09-24 12:24:28 -0500

    Create ErrorController that handles 404 and 500

---
+++ Recipebook/Controllers/ErrorController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Recipebook.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public IActionResult Index()
+        {
+            Response.StatusCode = 500;
+            return View();
+        }
+
+        public IActionResult NotFound()
+        {
+            Response.StatusCode = 404;
+            return View();
+        }
+    }
+}
