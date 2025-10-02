e2cb3b6e569742099599cf3d3f0d37f32d1c4b56
Trubo <78948819+truubo@users.noreply.github.com>
2025-10-02 10:07:45 -0500

    Added sorting to recipe List

---
+++ Recipebook/Models/SortType.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Recipebook.Models
+{
+    public enum SortType
+    {
+        [Display(Name = "Alphabetical (A-Z)")]
+        AlphabeticalAsc,
+
+        [Display(Name = "Alphabetical (Z-A)")]
+        AlphabeticalDesc,
+    }
+}
