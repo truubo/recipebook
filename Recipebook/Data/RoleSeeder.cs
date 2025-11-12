596e4df927031108c8ff80e97b5ef469cdcba69d
Jordan Tellekson <jordant022105@gmail.com>
2025-11-11 18:47:06 -0600

    [Tellekson/64]: Removed the hard-coded test values from the RoleSeeder, now its only job is to create the "Admin" role if it doesn't exist. Users can be manually given admin priviliges within SSMS

--- Recipebook/Data/RoleSeeder.cs
+++ Recipebook/Data/RoleSeeder.cs
@@ -7,37 +7,11 @@ namespace Recipebook.Data
         public static async Task SeedRolesAndAdminAsync(IServiceProvider serviceProvider)
         {
             var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-            var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
 
-            // Ensures the admin role exists
+            // Ensures admin role exists
             if (!await roleManager.RoleExistsAsync("Admin"))
             {
                 await roleManager.CreateAsync(new IdentityRole("Admin"));
-            }
-
-            // Creates a test admin account if it doesn’t exist
-            string adminEmail = "admin@test.com";
-            string adminPassword = "Test123!";
-
-            var adminUser = await userManager.FindByEmailAsync(adminEmail);
-            if (adminUser == null)
-            {
-                adminUser = new IdentityUser
-                {
-                    UserName = adminEmail,
-                    Email = adminEmail,
-                    EmailConfirmed = true
-                };
-
-                var result = await userManager.CreateAsync(adminUser, adminPassword);
-                if (result.Succeeded)
-                {
-                    await userManager.AddToRoleAsync(adminUser, "Admin");
-                }
-            }
-            else if (!await userManager.IsInRoleAsync(adminUser, "Admin"))
-            {
-                await userManager.AddToRoleAsync(adminUser, "Admin");
             }
         }
     }
