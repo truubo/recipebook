30729f5d3d27dd8747184eb93a7be62d7fa9a048
ASchulz <ASchulz@ITPROGLAPTOP04>
2025-11-20 12:12:44 -0600

    finished up carasaul

--- Recipebook/Controllers/HomeController.cs
+++ Recipebook/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Recipebook.Data;
 using Recipebook.Models;
 using System.Diagnostics;
 
@@ -7,15 +9,22 @@ namespace Recipebook.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            // Load recipes + votes; we'll filter further in the view
+            var recipes = _context.Recipe
+                .Include(r => r.RecipeVotes)
+                .ToList();
+
+            return View(recipes);
         }
 
         public IActionResult Privacy()
@@ -26,7 +35,10 @@ namespace Recipebook.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            });
         }
     }
 }
