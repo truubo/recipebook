ad21e2075391e9789fea2b7ff5e7aee6f3968a1c
Jordan Tellekson <jordant022105@gmail.com>
2025-11-21 11:59:04 -0600

    [Tellekson/78]: Two minor changes, adjusted spacing for recipe-title on home and index page. I forgot to commit this yesterday.

--- Recipebook/Controllers/RecipesController.cs
+++ Recipebook/Controllers/RecipesController.cs
@@ -111,8 +111,6 @@ namespace Recipebook.Controllers
 
         // -------------------------------- DETAILS --------------------------------
         // GET: Recipes/Details/5
-        // -------------------------------- DETAILS --------------------------------
-        // GET: Recipes/Details/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
